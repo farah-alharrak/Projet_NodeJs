c4775ef1b445d6c364d0866e137c259241681bd9
AL HARRAK FARAH <farah.alharrak2@gmail.com>
2021-05-31 18:14:33 +0000

    commit 6

--- respositories/users.js
+++ respositories/users.js
@@ -1,4 +1,13 @@
+//const { UniqueConstraintError } = require('sequelize/types')
+//const { SELECT } = require('sequelize/types/lib/query-types')
 const { User } = require('../models')
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize("farah", "root", "", {
+    dialect: "mysql",
+    host: "localhost"
+});
+
+
 module.exports = {
     getAllUsers() {
         return User.findAll()
@@ -15,3 +24,23 @@ module.exports = {
         deleteUser() { },
         // D'autres méthodes jugées utiles
         }
+
+        function getAdmins() {
+            sequelize.query("SELECT id from users where role='admin'").then(([results, metadata]) => {
+                console.log(results);
+              })
+        }
+        
+        function getAuthors() {
+            sequelize.query("SELECT username from users where role='author'").then(([results, metadata]) => {
+                console.log(results);
+              })
+        }
+
+        function getGuests() {
+            sequelize.query("SELECT id from users where role='guest'").then(([results, metadata]) => {
+                console.log(results);
+              })
+        }
+        
+      
--- routes/users.js
+++ routes/users.js
@@ -7,3 +7,12 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+const router = require('express').Router();
+const usersRepo = require('../repositories/users')
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+res.send(await usersRepo.getAllUsers())
+});
+module.exports = router;
