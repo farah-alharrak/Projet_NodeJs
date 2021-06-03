2d9eceeda7c525ebef59d8a5fe6098777aeb8e23
AL HARRAK FARAH <farah.alharrak2@gmail.com>
2021-06-03 16:11:40 +0000

    les routes

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,88 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router(); 
+const user = require('../models/user.js');
+const usersRepo = require('../respositories/users.js')
+const auth = require('../middleware/auth.js');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/all', async function(req, res) {
 
-module.exports = router;
-
-
-const router = require('express').Router();
-const usersRepo = require('../repositories/users')
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
 res.send(await usersRepo.getAllUsers())
+
 });
+
+
+
+/// par rôle
+
+router.get('/:role', (req, res) => {
+  switch (req.params.role) {    // req.params object is used because it has access to all the parameters passed in the url
+    case "admin":
+      usersRepo.getAdmins()
+        .then(users => res.status(200).json(users))
+        .catch(err => console.log(err));
+      break;
+    case "author":
+      usersRepo.getAuthors()
+        .then(users => res.status(200).json(users))
+        .catch(err => console.log(err));
+      break;
+    case "guest":
+      usersRepo.getGuests()
+        .then(users => res.status(200).json(users))
+        .catch(err => console.log(err));
+      break;
+  } 
+})
+
+/// par identifiant
+
+router.get('/id/:id', (req, res) => {
+usersRepo.getUser(req.params.id)
+  .then(user => res.status(200).json(user))
+  .catch(err => console.log(err));
+})
+
+/// par email
+
+router.get('/email/:email', (req, res) => {
+usersRepo.getUserByEmail(req.params.email)
+  .then(user => res.status(200).json(user))
+  .catch(err => console.log(err));
+})
+
+/// add user
+
+router.post('/add', auth, (req, res) => {
+const {username, email, password, role} = req.body;
+
+if (!username || !email) {res.status(400).json({message: "veuillez entrer tous les elements"})}
+else {
+          const newUser = {
+            username,
+            email,
+            password, 
+            role,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          }
+        
+          usersRepo.addUser(newUser);
+          res.status(200).redirect("http://localhost:3000/");  /// rediriger vers le port 3000
+}
+})
+
+router.post('/:id', auth, (req, res) => {
+usersRepo.updateUser(req.params.id, req.body);
+res.status(200).redirect("http://localhost:3000/");
+})
+
+
+router.delete('/:id', auth, (req, res) => {
+usersRepo.deleteUser(req.params.id);
+res.status(200).redirect("http://localhost:3000/");
+})
+
+
+
 module.exports = router;
